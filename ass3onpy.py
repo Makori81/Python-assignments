7c791a8aefc8ecd7d63e626aff4d3e85cb990d4b
makori <143006692+Makori81@users.noreply.github.com>
2025-04-16 00:51:38 +0300

    Update ass3onpy.py

--- ass3onpy.py
+++ ass3onpy.py
@@ -1,32 +1,32 @@
 def main():
     #Prompt user to enter bookID,dueDate,returnDate
-    bookID = input("Enter bookID:")
-    dueDate = int(input("Enter dueDate(as a num):"))
-    returnDate = int(input("Enter returnDate(as a num):"))
+    book_ID = input("Enter book_ID:")
+    due_Date = int(input("Enter due_Date(as a num):"))
+    return_Date = int(input("Enter return_Date(as a num):"))
 
     # Calculate daysOverdue
-    daysOverdue = returnDate - dueDate
+    days_Overdue = return_Date - due_Date
  
     #determine fineRate and fineAmount
-    if daysOverdue <= 7:
-        fineRate = 20 
-    elif daysOverdue >=8 and daysOverdue <=14:
-        fineRate = 50 
-    elif daysOverdue >=15:
-        fineRate = 100
+    if days_Overdue <= 7:
+        fine_Rate = 20 
+    elif days_Overdue >=8 and days_Overdue <=14:
+        fine_Rate = 50 
+    elif days_Overdue >=15:
+        fine_Rate = 100
     else:
-        fineRate = 0
+        fine_Rate = 0
 
-    fineAmount = fineRate * daysOverdue
+    fine_Amount = fine_Rate * days_Overdue
 
     # Display results
     print("\n--- Book Details ---")
-    print(f"1. Book ID: {bookID}")
-    print(f"2. Due Date: {dueDate}")
-    print(f"3. Return Date: {returnDate}")
-    print(f"4. Days Overdue: {daysOverdue}")
-    print(f"5. Fine Rate: Ksh {fineRate} per day")
-    print(f"6. Fine Amount: Ksh {fineAmount}") 
+    print(f"1. Book ID: {book_ID}")
+    print(f"2. Due Date: {due_Date}")
+    print(f"3. Return Date: {return_Date}")
+    print(f"4. Days Overdue: {days_Overdue}")
+    print(f"5. Fine Rate: Ksh {fine_Rate} per day")
+    print(f"6. Fine Amount: Ksh {fine_Amount}") 
    
 main()
         
